7fd0b50fe40ab1966eadea655f32fa8d88e4a6dc
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-07-30 20:03:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-  aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-  password = "admin@1234567"
 
 
 resource "aws_s3_bucket" "example" {
