9964c875f49b0475187f372d22901c7c7187a43a
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-07-30 13:34:39 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import boto3
+
+aws_access_key_id = "AKIAEXAMPLEKEY12345678"
+aws_secret_access_key = "abcd1234examplekey+moredata"
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+
+s3 = session.resource('s3')
+bucket = s3.Bucket('vault-radar-demo')
