3977cf3e7792e876808b3ef9c9417cc86108eb7a
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-07-30 20:02:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region     = "us-west-2"
-  aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-  aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-  password = "admin@1234567"
+  aws_access_key_id = "AKIAIOSFODNN7EXAMPE"
+  aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLY"
 
 
 resource "aws_s3_bucket" "example" {
   bucket = "vault-radar-test-bucket"
   acl    = "private"
-  password = "mahi@12345"
+ 
 }
