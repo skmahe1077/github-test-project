0f5609b3d21395d0a372376c05b49935aa51a499
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-08-09 12:14:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import boto3
 
 aws_access_key_id = "AKIAEXAMPLEKEY12345678"
 aws_secret_access_key = "abcd1234examplekey+moredata"
+password = abctdg
 
 session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
