d8f72b20519c95bce792694282d1ed6e0154b59d
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-08-10 11:28:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,12 @@
 provider "aws" {
   region     = "us-west-2"
-  password = mahi@123
-
   aws_access_key_id = "AKIAEXAMPLEKEY12345678"
   aws_secret_access_key = "abcd1234examplekey+moredata"
-  password = Mahi@1234
 
 
 
 resource "aws_s3_bucket" "example" {
   bucket = "vault-radar-test-bucket"
   acl    = "private"
-  password = mahi@123cvvghfcghgcvhgcgcgvhv
  
 }
